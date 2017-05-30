57804aa7c8bc7546d8538060b96fd614bc1ade55
Alexander Taxter <Sharquo@gmail.com>
2017-05-30 20:05:47 +0100

    Trying to set up some Automapping features.

---
+++ lpc_tiled_tileset.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castle_lightsources" tilewidth="160" tileheight="128" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="160" height="128" source="LPC Base Assets/tiles/castle_lightsources.png"/>
+ </tile>
+</tileset>
